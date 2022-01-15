a789ace98d157f3b80390abcf82e91ac7212e65a
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-14 16:43:36 -0800

    convert AddTodoListComponent into an arrow function

--- src/components/AddTodoListComponent.jsx
+++ src/components/AddTodoListComponent.jsx
@@ -4,10 +4,10 @@ import React from 'react'
 //  ADD TO DO LIST COMPONENT
 ///////////////////////////////////////////////////////////////
 
-function AddTodoListComponent() {
+const AddTodoListComponent= () => {
     return (
         <div>
-            
+            <h1>Add An Item on the To Do List:</h1>
         </div>
     )
 }
