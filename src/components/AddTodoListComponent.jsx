e1243956751663c9cf3340316f511b6fd3a5cc25
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-14 16:38:37 -0800

    create AddTodoListComponent

---
+++ src/components/AddTodoListComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+///////////////////////////////////////////////////////////////
+//  ADD TO DO LIST COMPONENT
+///////////////////////////////////////////////////////////////
+
+function AddTodoListComponent() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default AddTodoListComponent
