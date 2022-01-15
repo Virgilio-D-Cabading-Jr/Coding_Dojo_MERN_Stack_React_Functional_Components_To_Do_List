7fa5e9884dfac469e95680a4621f83c51f87a5dc
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-15 08:02:23 -0800

    add table to display to do list at ShowTodoListComponent

--- src/components/ShowTodoListComponent.jsx
+++ src/components/ShowTodoListComponent.jsx
@@ -8,6 +8,22 @@ const ShowTodoListComponent = () => {
     return (
         <div className="bg-white p-2 rounded">
             <h2>To Do List:</h2>
+            <table className='table'>
+                <thead className="thead-dark">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col-3">Task</th>
+                        <th scope="col">Complete</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Item to be done</td>
+                        <td>CheckBox</td>
+                        <td>Delete Button</td>
+                    </tr>
+                </thead>
+            </table>
         </div>
     )
 }
