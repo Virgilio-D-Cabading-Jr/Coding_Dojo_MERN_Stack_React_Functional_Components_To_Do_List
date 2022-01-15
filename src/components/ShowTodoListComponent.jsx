63836137ec4872239844dc735224e217fe2ad2ce
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-15 06:56:43 -0800

    create ShowTodoListComponent

---
+++ src/components/ShowTodoListComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+///////////////////////////////////////////////////////////////
+//  SHOW TO DO LIST COMPONENT
+///////////////////////////////////////////////////////////////
+
+const ShowTodoListComponent = () => {
+    return (
+        <div className="bg-white p-2 rounded">
+            <h2>To Do List:</h2>
+        </div>
+    )
+}
+
+export default ShowTodoListComponent
